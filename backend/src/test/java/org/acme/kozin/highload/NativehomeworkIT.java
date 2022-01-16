22e775e0a93b61ad109edc0e7b44fd9a70106c46
Your Name <kozyyy@yandex.ru>
2022-01-17 02:40:27 +0300

    init backend

---
+++ backend/src/test/java/org/acme/kozin/highload/NativehomeworkIT.java
@@ -0,0 +1,9 @@
+package org.acme.kozin.highload;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativehomeworkIT extends homeworkTest {
+
+    // Execute the same tests but in native mode.
+}
